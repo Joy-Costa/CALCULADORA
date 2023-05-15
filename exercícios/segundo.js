1c6a92b3ba4fb558bd1d581838448659cc41c54f
Joy-Costa <129955914+Joy-Costa@users.noreply.github.com>
2023-05-15 11:05:46 -0300

    Add files via upload

--- exercícios/segundo.js
+++ exercícios/segundo.js
@@ -1,11 +1,9 @@
 var N1 = document.getElementById("N1");
-var N2 = document.getElementById("N2");
 var resultado = document.getElementById("resultado");
-var multiplicar;
+var real = 4.92;
 
 function MULTIPLICAR()
 { 
-    multiplicar = parseFloat (N1.value) * parseFloat (N2.value);
-    resultado.innerHTML = multiplicar;
+    resultado.innerHTML = parseInt (N1.value) * real;
 
 }
